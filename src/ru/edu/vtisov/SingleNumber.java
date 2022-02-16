3a64908dce90d457bec7d025b065cf17cda24ea2
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-02-16 23:49:57 +0300

    add SingleNumber

---
+++ src/ru/edu/vtisov/SingleNumber.java
@@ -0,0 +1,10 @@
+package ru.edu.vtisov;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int mask = 0;
+        for (int i: nums)
+            mask ^= i;
+        return mask;
+    }
+}
