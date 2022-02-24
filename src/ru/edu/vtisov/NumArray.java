14a0119d5ef735614a9cacbaae820c1b6872fa9c
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-02-24 19:30:34 +0300

    add NumArray

---
+++ src/ru/edu/vtisov/NumArray.java
@@ -0,0 +1,17 @@
+package ru.edu.vtisov;
+
+public class NumArray {
+
+    private final int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
