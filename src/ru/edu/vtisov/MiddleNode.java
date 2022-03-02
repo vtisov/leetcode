adbf965b21f09da36182547d3b94466377f13d82
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-03-02 19:07:46 +0300

    add Middle Node

---
+++ src/ru/edu/vtisov/MiddleNode.java
@@ -0,0 +1,13 @@
+package ru.edu.vtisov;
+
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
