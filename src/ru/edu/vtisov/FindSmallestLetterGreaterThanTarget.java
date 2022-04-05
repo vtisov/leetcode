7193a3cd922d2a095a51c6e3a5a384be47a2877b
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-04-05 18:29:08 +0300

    FindSmallestLetterGreaterThanTarget

---
+++ src/ru/edu/vtisov/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,14 @@
+package ru.edu.vtisov;
+
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target < letters[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+        return letters[left % letters.length];
+    }
+}
