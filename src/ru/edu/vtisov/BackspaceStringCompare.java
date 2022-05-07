784797e9dd3ea1a506d9a603dfc563248cbbdd99
Vadim Tisov <vtisov@MacBook-Pro-Vadim.local>
2022-05-07 11:27:14 +0300

    fix BackspaceStringCompare

--- src/ru/edu/vtisov/BackspaceStringCompare.java
+++ src/ru/edu/vtisov/BackspaceStringCompare.java
@@ -1,34 +1,27 @@
 package ru.edu.vtisov;
 
 public class BackspaceStringCompare {
+    private int getCurrentIndex(String s, int current) {
+        int i = current;
+        int toSkip = 0;
+        while(i >= 0) {
+            if (s.charAt(i) == '#')
+                toSkip++;
+            else if(toSkip > 0)
+                toSkip--;
+            else
+                break;
+            i--;
+        }
+        return i;
+    }
+
     public boolean backspaceCompare(String s, String t) {
         int posS = s.length() - 1;
         int posT = t.length() - 1;
-        int skipS = 0;
-        int skipT = 0;
         while (posS >= 0 || posT >= 0) {
-            while(posS >= 0) {
-                if (s.charAt(posS) == '#') {
-                    skipS++;
-                    posS--;
-                } else if(skipS > 0) {
-                    skipS--;
-                    posS--;
-                } else {
-                    break;
-                }
-            }
-            while(posT >= 0) {
-                if (t.charAt(posT) == '#') {
-                    skipT++;
-                    posT--;
-                } else if(skipT > 0) {
-                    skipT--;
-                    posT--;
-                } else {
-                    break;
-                }
-            }
+            posS = getCurrentIndex(s, posS);
+            posT = getCurrentIndex(t, posT);
             if (posS < 0 || posT < 0) break;
             if (s.charAt(posS) != t.charAt(posT)) {
                 return false;
