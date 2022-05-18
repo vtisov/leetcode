6586623a39f7b1bf4c901ed9cdf2af504fde13b6
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-05-18 10:48:47 +0300

    add Subsets

---
+++ src/ru/edu/vtisov/backtracking/Subsets.java
@@ -0,0 +1,26 @@
+package ru.edu.vtisov.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+    private final List<List<Integer>> results = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums.length == 0) return new ArrayList<>();
+        backtracking(nums, 0, new ArrayList<>());
+        return results;
+    }
+
+    private void backtracking(int[] nums,
+                              int start,
+                              List<Integer> current) {
+        results.add(new ArrayList<>(current));
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtracking(nums, i + 1, current);
+            current.remove(current.size() - 1);
+        }
+    }
+}
