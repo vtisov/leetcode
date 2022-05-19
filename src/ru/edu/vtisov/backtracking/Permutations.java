ea77f4764d1995e57ed97c97235006eeed78be5d
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-05-19 11:26:07 +0300

    add Permutations

---
+++ src/ru/edu/vtisov/backtracking/Permutations.java
@@ -0,0 +1,34 @@
+package ru.edu.vtisov.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Permutations {
+
+    private final List<List<Integer>> results = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        backtrack(nums, 0);
+        return results;
+    }
+
+    private void backtrack(int[] nums, int n) {
+        if (n == nums.length) {
+            results.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+            return;
+        }
+        for(int i = n; i < nums.length; i++) {
+            swap(nums, n, i);
+            backtrack(nums, n + 1);
+            swap(nums, n, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+}
