73efc4663f91369e97c4cd061b9fecf80b914411
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-05-23 10:33:47 +0300

    add HappyNumber

---
+++ src/ru/edu/vtisov/hashtable/HappyNumber.java
@@ -0,0 +1,35 @@
+package ru.edu.vtisov.hashtable;
+
+// https://leetcode.com/problems/happy-number/
+
+public class HappyNumber {
+
+    private int next(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return answer;
+    }
+
+    public boolean isHappy(int n) {
+        int slow = next(n);
+        int fast = next(next(n));
+        while(slow != 1 && slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        return slow == 1;
+    }
+
+    // public boolean isHappy(int n) {
+    //     Set<Integer> seen = new HashSet<>();
+    //     while(n != 1 && !seen.contains(n)) {
+    //         seen.add(n);
+    //         n = next(n);
+    //     }
+    //     return n == 1;
+    // }
+
+}
