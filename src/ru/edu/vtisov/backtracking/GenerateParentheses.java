5c2224b70b272b4a8725c3a8c28225ef6dc2ffdf
vtisov.dev@gmail.com <Totrozob93>
2022-05-27 09:09:58 +0300

    add GenerateParentheses

---
+++ src/ru/edu/vtisov/backtracking/GenerateParentheses.java
@@ -0,0 +1,19 @@
+package ru.edu.vtisov.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    private final List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        backtrack("", n, 0);
+        return result;
+    }
+
+    private void backtrack(String current, int toOpen, int toClose) {
+        if (toOpen == 0 && toClose == 0) result.add(current);
+        if (toOpen > 0) backtrack(current + "(", toOpen - 1, toClose + 1);
+        if (toClose > 0) backtrack(current + ")", toOpen, toClose - 1);
+    }
+}
