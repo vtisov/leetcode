a51fcf46f7e832bedc326bdc61d6716f8542a195
vtisov.dev@gmail.com <Totrozob93>
2022-06-04 10:06:08 +0300

    add BestTimeToBuyAndSellStock

---
+++ src/ru/edu/vtisov/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package ru.edu.vtisov.array;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
+}
