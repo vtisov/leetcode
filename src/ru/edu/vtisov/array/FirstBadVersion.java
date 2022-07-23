22acd57ec0460195a5bea140199611caaa641135
Vadim Tisov <vtisov@MBP-Vadim.N4>
2022-07-23 16:39:42 +0300

    add FirstBadVersion

---
+++ src/ru/edu/vtisov/array/FirstBadVersion.java
@@ -0,0 +1,26 @@
+package ru.edu.vtisov.array;
+
+public class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int pivot = left + (right - left) / 2;
+            if (isBadVersion(pivot)) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+
+        }
+        return left;
+    }
+}
+
+class VersionControl {
+
+    boolean isBadVersion(int n) {
+        return true;
+    }
+
+}
