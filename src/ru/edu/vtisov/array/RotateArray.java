70865811ba33342f1896e81b8a9120ed56d74a40
vtisov.dev@gmail.com <Totrozob93>
2022-07-25 21:43:16 +0300

    add RotateArray

---
+++ src/ru/edu/vtisov/array/RotateArray.java
@@ -0,0 +1,24 @@
+package ru.edu.vtisov.array;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int rotate = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, rotate - 1);
+        reverse(nums, rotate, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while(left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
