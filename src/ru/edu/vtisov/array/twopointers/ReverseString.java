ff7a8890d0007878c475b33b0b5a5c4bea95338c
vtisov.dev@gmail.com <Totrozob93>
2022-07-27 21:50:30 +0300

    add ReverseString

---
+++ src/ru/edu/vtisov/array/twopointers/ReverseString.java
@@ -0,0 +1,15 @@
+package ru.edu.vtisov.array.twopointers;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
