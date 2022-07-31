5992189000ab37ebc3e8e37f1e0c0d0f5e7aeb37
vtisov.dev@gmail.com <Totrozob93>
2022-07-31 17:04:56 +0300

    add LongestSubstringWithoutRepeatingCharacters

---
+++ src/ru/edu/vtisov/array/twopointers/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+package ru.edu.vtisov.array.twopointers;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int n = s.length();
+        int[] alphabet = new int[128];
+        for (int i = 0, j = 0; j < n; j++) {
+            char c = s.charAt(j);
+            i = Math.max(i, alphabet[c]);
+            max = Math.max(max, j - i + 1);
+            alphabet[c] = j + 1;
+        }
+        return max;
+    }
+
+}
