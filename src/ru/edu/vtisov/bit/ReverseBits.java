aa77c196ab03d155ca2297c4710f6e675615e06b
vtisov.dev <vtisov.dev@gmail.com>
2022-08-19 13:36:09 +0500

    add ReverseBits

---
+++ src/ru/edu/vtisov/bit/ReverseBits.java
@@ -0,0 +1,13 @@
+package ru.edu.vtisov.bit;
+
+public class ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int answer = 0;
+        for (int i = 31; i >= 0; i--) {
+            int current = (n >> i) & 1;
+            if (current != 0) answer += (1 << (31 - i));
+        }
+        return answer;
+    }
+}
