eb6fc29d1eb12fea9f8c3d6e9c4761231c1c5d8a
vtisov.dev <vtisov.dev@gmail.com>
2022-08-25 16:22:13 +0300

    add FindFirstAndLastPositionOfElementШnSortedArray

---
+++ src/ru/edu/vtisov/array/binarysearch/FindFirstAndLastPositionOfElementШnSortedArray.java
@@ -0,0 +1,28 @@
+package ru.edu.vtisov.array.binarysearch;
+
+public class FindFirstAndLastPositionOfElementШnSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target, true);
+        int second = binarySearch(nums, target, false);
+        return new int[] {first, second};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean isLeft) {
+        int result = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                if (isLeft) end = mid - 1;
+                else start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+}
