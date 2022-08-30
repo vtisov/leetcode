64509788d9e3aad450871b3b61b526d3ea9a0b5c
vtisov.dev <vtisov.dev@gmail.com>
2022-08-30 11:00:08 +0300

    add ContainerWithMostWater

---
+++ src/ru/edu/vtisov/array/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,13 @@
+package ru.edu.vtisov.array.twopointers;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j;) {
+            max = Math.max(Math.min(height[i], height[j]) * (j - i), max);
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+        return max;
+    }
+}
