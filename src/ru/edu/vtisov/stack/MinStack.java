f7462d1d866c617ee20b9b6da574d089ea53a14f
vtisov.dev <vtisov.dev@gmail.com>
2022-08-31 21:35:00 +0300

    add MinStack

---
+++ src/ru/edu/vtisov/stack/MinStack.java
@@ -0,0 +1,32 @@
+package ru.edu.vtisov.stack;
+
+import java.util.LinkedList;
+
+public class MinStack {
+
+    private final LinkedList<Integer> stack;
+    private final LinkedList<Integer> min;
+    
+    public MinStack() {
+        this.stack = new LinkedList<>();
+        this.min = new LinkedList<>(); 
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        min.add(Math.min(val, min.size() > 0 ? min.getLast() : val));
+    }
+    
+    public void pop() {
+        min.removeLast();
+        stack.removeLast();
+    }
+    
+    public int top() {
+        return stack.getLast();
+    }
+    
+    public int getMin() {
+        return min.getLast();
+    }
+}
