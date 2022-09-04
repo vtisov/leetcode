167faefe4fe142a4907216c9aa0042a5b3e9df06
vtisov.dev <vtisov.dev@gmail.com>
2022-09-04 11:32:48 +0300

    add MinimumSizeSubarraySum

---
+++ src/ru/edu/vtisov/array/twopointers/MinimumSizeSubarraySum.java
@@ -0,0 +1,16 @@
+package ru.edu.vtisov.array.twopointers;
+
+public class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                min = Math.min(min, right - left + 1);
+                sum -= nums[left++];
+            }        
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
