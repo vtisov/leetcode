32383900f8f15fe2fcbd7d88f3a55ddb592034b1
vtisov.dev <vtisov.dev@gmail.com>
2022-09-06 10:53:58 +0300

    add NumberOfIslands

---
+++ src/ru/edu/vtisov/tree/dfs/NumberOfIslands.java
@@ -0,0 +1,26 @@
+package ru.edu.vtisov.tree.dfs;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    visitIsland(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void visitIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        visitIsland(grid, i + 1, j);
+        visitIsland(grid, i - 1, j);
+        visitIsland(grid, i, j + 1);
+        visitIsland(grid, i, j - 1);
+    }
+}
