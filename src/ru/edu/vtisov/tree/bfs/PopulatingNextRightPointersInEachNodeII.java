456788cbb01841987b0c7685b10a0a638e216e21
vtisov.dev <vtisov.dev@gmail.com>
2022-09-08 16:45:24 +0300

    add PopulatingNextRightPointersInEachNodeII

---
+++ src/ru/edu/vtisov/tree/bfs/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,27 @@
+package ru.edu.vtisov.tree.bfs;
+
+import ru.edu.vtisov.util.Node;
+
+import java.util.LinkedList;
+
+public class PopulatingNextRightPointersInEachNodeII {
+    public Node connect(Node root) {
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Node> temp = new LinkedList<>();
+            Node stub = new Node();
+            Node previous = stub;
+            while(!queue.isEmpty()) {
+                Node current = queue.removeFirst();
+                if (current == null) continue;
+                temp.add(current.left);
+                temp.add(current.right);
+                previous.next = current;
+                previous = current;
+            }
+            queue = temp;
+        }
+        return root;
+    }
+}
