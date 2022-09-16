9d86df680e884f6b7b6c5e6a7035e8d80416c289
vtisov.dev <vtisov.dev@gmail.com>
2022-09-16 22:35:10 +0300

    fix second-highest-salary.sql

--- sql/union/second-highest-salary.sql
+++ sql/union/second-highest-salary.sql
@@ -1,5 +1,5 @@
 -- https://leetcode.com/problems/second-highest-salary
-select ifnull((select salary
+select ifnull((select distinct salary
          from employee
          order by salary desc
          limit 1 offset 1), null) as SecondHighestSalary
