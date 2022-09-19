b375804ce5244d043bf6a154208fcc4c72611e2e
vtisov.dev <vtisov.dev@gmail.com>
2022-09-19 17:20:17 +0300

    add consecutive-numbers.sql

--- sql/functions/reformat-department-table.sql
+++ sql/functions/reformat-department-table.sql
@@ -1,19 +1,17 @@
 # https://leetcode.com/problems/reformat-department-table
 select id,
-
-       max(case when month = 'Jan' then revenue else null end) as Jan_Revenue,
-       max(case when month = 'Feb' then revenue else null end) as Feb_Revenue,
-       max(case when month = 'Mar' then revenue else null end) as Mar_Revenue,
-       max(case when month = 'Apr' then revenue else null end) as Apr_Revenue,
-       max(case when month = 'May' then revenue else null end) as May_Revenue,
-       max(case when month = 'Jun' then revenue else null end) as Jun_Revenue,
-       max(case when month = 'Jul' then revenue else null end) as Jul_Revenue,
-       max(case when month = 'Aug' then revenue else null end) as Aug_Revenue,
-       max(case when month = 'Sep' then revenue else null end) as Sep_Revenue,
-       max(case when month = 'Oct' then revenue else null end) as Oct_Revenue,
-       max(case when month = 'Nov' then revenue else null end) as Nov_Revenue,
-       max(case when month = 'Dec' then revenue else null end) as Dec_Revenue
-
+       max(IF(month = 'Jan', revenue, null)) as Jan_Revenue,
+       max(IF(month = 'Feb', revenue, null)) as Feb_Revenue,
+       max(IF(month = 'Mar', revenue, null)) as Mar_Revenue,
+       max(IF(month = 'Apr', revenue, null)) as Apr_Revenue,
+       max(IF(month = 'May', revenue, null)) as May_Revenue,
+       max(IF(month = 'Jun', revenue, null)) as Jun_Revenue,
+       max(IF(month = 'Jul', revenue, null)) as Jul_Revenue,
+       max(IF(month = 'Aug', revenue, null)) as Aug_Revenue,
+       max(IF(month = 'Sep', revenue, null)) as Sep_Revenue,
+       max(IF(month = 'Oct', revenue, null)) as Oct_Revenue,
+       max(IF(month = 'Nov', revenue, null)) as Nov_Revenue,
+       max(IF(month = 'Dec', revenue, null)) as Dec_Revenue
 from
     Department
 group by 1
