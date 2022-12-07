87f3f8cee5697c4b123e45cda86c69a524aceb28
vtisov.dev <vtisov.dev@gmail.com>
2022-12-07 23:01:05 +0300

    update HappyNumber

--- src/ru/edu/vtisov/hashtable/HappyNumber.java
+++ src/ru/edu/vtisov/hashtable/HappyNumber.java
@@ -2,9 +2,19 @@ package ru.edu.vtisov.hashtable;
 
 // https://leetcode.com/problems/happy-number/
 
-// TODO
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
 
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while(n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = next(n);
+        }
+        return n == 1;
+    }
     private int next(int n) {
         int answer = 0;
         while (n > 0) {
@@ -14,23 +24,14 @@ public class HappyNumber {
         return answer;
     }
 
-    public boolean isHappy(int n) {
-        int slow = next(n);
-        int fast = next(next(n));
-        while(slow != 1 && slow != fast) {
-            slow = next(slow);
-            fast = next(next(fast));
-        }
-        return slow == 1;
-    }
-
-    // public boolean isHappy(int n) {
-    //     Set<Integer> seen = new HashSet<>();
-    //     while(n != 1 && !seen.contains(n)) {
-    //         seen.add(n);
-    //         n = next(n);
-    //     }
-    //     return n == 1;
-    // }
+//    public boolean isHappy(int n) {
+//        int slow = next(n);
+//        int fast = next(next(n));
+//        while(slow != 1 && slow != fast) {
+//            slow = next(slow);
+//            fast = next(next(fast));
+//        }
+//        return slow == 1;
+//    }
 
 }
