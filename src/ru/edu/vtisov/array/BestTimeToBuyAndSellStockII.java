73ebc532edf85c59a401e8653292119bdf1ec4e7
vtisov.dev <vtisov.dev@gmail.com>
2023-05-11 12:28:50 +0600

    modify BestTimeToBuyAndSellStockII

--- src/ru/edu/vtisov/array/BestTimeToBuyAndSellStockII.java
+++ src/ru/edu/vtisov/array/BestTimeToBuyAndSellStockII.java
@@ -7,7 +7,9 @@ public class BestTimeToBuyAndSellStockII {
     public int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
         }
         return profit;
     }
