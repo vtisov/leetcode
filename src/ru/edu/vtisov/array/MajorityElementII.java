fbc6275448d1f8e725a2895615bd26eca2d15b42
vtisov.dev@gmail.com <Totrozob93>
2023-10-05 19:58:46 +0600

    add MajorityElementII

--- src/ru/edu/vtisov/array/MajorityElementII.java
+++ src/ru/edu/vtisov/array/MajorityElementII.java
@@ -8,39 +8,39 @@ import java.util.List;
 public class MajorityElementII {
 
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0;
-        int count2 = 0;
+        int vote1 = 0;
+        int vote2 = 0;
         int candidate1 = 0;
         int candidate2 = 0;
 
         for (int num: nums) {
             if (num == candidate1) {
-                count1++;
+                vote1++;
             } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
+                vote2++;
+            } else if (vote1 == 0) {
                 candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
+                vote1 = 1;
+            } else if (vote2 == 0) {
                 candidate2 = num;
-                count2 = 1;
+                vote2 = 1;
             } else {
-                count1--;
-                count2--;
+                vote1--;
+                vote2--;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
+        vote1 = 0;
+        vote2 = 0;
 
         for (int num: nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+            if (num == candidate1) vote1++;
+            else if (num == candidate2) vote2++;
         }
 
         List<Integer> result = new ArrayList<>();
-        if (count1 > nums.length / 3) result.add(candidate1);
-        if (count2 > nums.length / 3) result.add(candidate2);
+        if (vote1 > nums.length / 3) result.add(candidate1);
+        if (vote2 > nums.length / 3) result.add(candidate2);
         return result;
     }
 }
