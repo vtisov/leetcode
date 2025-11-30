b0e12da3c345608e205440e050b7767583cbb87e
vtisov <72997625+vtisov@users.noreply.github.com>
2025-11-30 12:00:01 +0500

    Update minCostClimbingStairs.go

--- src/ru/edu/vtisov/dp/minCostClimbingStairs.go
+++ src/ru/edu/vtisov/dp/minCostClimbingStairs.go
@@ -5,6 +5,22 @@ func minCostClimbingStairs(cost []int) int {
         return cost[0]
     }
     n := len(cost)
+    dp1 := cost[0]
+    dp2 := cost[1]
+    for i := 2; i < n; i++ {
+        temp := min(dp1, dp2) + cost[i]
+        dp1 = dp2
+        dp2 = temp
+    }
+    return min(dp1, dp2)
+}
+
+/*
+func minCostClimbingStairs(cost []int) int {
+    if len(cost) == 1 {
+        return cost[0]
+    }
+    n := len(cost)
     memo := make([]int, n)
     memo[0] = cost[0]
     memo[1] = cost[1]
@@ -13,3 +29,4 @@ func minCostClimbingStairs(cost []int) int {
     }
     return min(memo[n - 1], memo[n - 2])
 }
+*/
