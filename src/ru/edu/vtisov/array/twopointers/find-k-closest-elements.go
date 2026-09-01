e92e99b769ddfd9ab56666a593060e3b79eb57b4
vtisov <72997625+vtisov@users.noreply.github.com>
2026-09-01 17:52:46 +0500

    Create find-k-closest-elements.go

---
+++ src/ru/edu/vtisov/array/twopointers/find-k-closest-elements.go
@@ -0,0 +1,23 @@
+func findClosestElements(arr []int, k int, x int) []int {
+    result := []int{}
+
+    left := 0
+    right := len(arr) - k
+
+    for left < right {
+        mid := left + (right-left)/2
+        if x - arr[mid] > arr[mid + k] - x {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    for i := left; i < left + k; i++ {
+        result = append(result, arr[i])
+    } 
+
+    return result
+}
+
+   
