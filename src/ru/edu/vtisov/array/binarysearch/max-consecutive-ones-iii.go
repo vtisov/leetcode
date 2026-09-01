4b059bb38834753e5b7a7fab70f25fff2a7f1ede
vtisov <72997625+vtisov@users.noreply.github.com>
2026-09-01 17:45:04 +0500

    Create max-consecutive-ones-iii.go

---
+++ src/ru/edu/vtisov/array/binarysearch/max-consecutive-ones-iii.go
@@ -0,0 +1,19 @@
+func longestOnes(nums []int, k int) int {
+    i := 0
+    j := 0
+    result := 0
+    cur := 0
+
+    for ; j < len(nums); j++ {
+        if nums[j] == 0 {
+            cur++
+        }
+        for ; cur > k; i++ {
+            if nums[i] == 0 {
+                cur--
+            }
+        }
+        result = max(result, j - i + 1)
+    }
+    return result
+}
