2f8592c70cf93d683ab14858a2ee8688e453e656
vtisov <72997625+vtisov@users.noreply.github.com>
2026-09-01 17:54:19 +0500

    Create summary-ranges.go

---
+++ src/ru/edu/vtisov/array/summary-ranges.go
@@ -0,0 +1,29 @@
+func summaryRanges(nums []int) []string {
+    results := []string{}
+    if len(nums) == 0 {
+        return results
+    }
+    if len(nums) == 1 {
+        results = append(results, strconv.Itoa(nums[0]))
+        return results
+    }
+    min := nums[0]
+    max := nums[0]
+    for i := 1; i < len(nums); i++ {
+        if nums[i] - max > 1 {
+            if max == min {
+                results = append(results, strconv.Itoa(min))
+            } else {
+                results = append(results, strconv.Itoa(min) + "->" + strconv.Itoa(max))
+            }
+            min = nums[i]
+        }
+        max = nums[i]
+    }
+    if max == min {
+        results = append(results, strconv.Itoa(min))
+    } else {
+        results = append(results, strconv.Itoa(min) + "->" + strconv.Itoa(max))
+    }
+    return results
+}
