7bd6f9faf371ac47b2a931e12849c307bfc9fd01
vtisov <72997625+vtisov@users.noreply.github.com>
2026-09-01 17:47:58 +0500

    Create department-top-three-salaries.sql

---
+++ sql/rank/department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select Department, Employee, Salary
+from (select d.name as Department,
+    e.name as Employee,
+    e.salary as Salary,
+    DENSE_RANK() OVER (partition by d.id order by salary desc) as rnk
+from Employee e join Department d on e.departmentId = d.id)
+where rnk <= 3;
