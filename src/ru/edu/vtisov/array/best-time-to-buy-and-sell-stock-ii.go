a15eec4df34191f9eafba9be95bdac37e9d92bde
vtisov.dev <vtisov.dev@gmail.com>
2026-09-02 23:15:48 +0500

    add best-time-to-buy-and-sell-stock-ii

---
+++ src/ru/edu/vtisov/array/best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,11 @@
+package array
+
+func maxProfit(prices []int) int {
+	result := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			result += prices[i] - prices[i-1]
+		}
+	}
+	return result
+}
