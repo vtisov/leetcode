ef59c7e540b7897cb2b3da3dda3e9e4e26ff4617
vtisov.dev <vtisov.dev@gmail.com>
2026-09-04 14:36:46 +0500

    add encode-and-decode-strings.go

---
+++ src/ru/edu/vtisov/array/encode-and-decode-strings.go
@@ -0,0 +1,34 @@
+package array
+
+import "strings"
+
+type Codec struct{}
+
+func (Codec) Encode(strs []string) string {
+	var buffer = strings.Builder{}
+	for _, str := range strs {
+		buffer.WriteString(strings.ReplaceAll(str, "/", "//"))
+		buffer.WriteString("/:")
+	}
+	return buffer.String()
+}
+
+func (Codec) Decode(s string) []string {
+	var result = []string{}
+	var buffer = strings.Builder{}
+
+	for i := 0; i < len(s); {
+		if i+1 < len(s) && s[i] == '/' && s[i+1] == '/' {
+			buffer.WriteByte('/')
+			i += 2
+		} else if i+1 < len(s) && s[i] == '/' && s[i+1] == ':' {
+			result = append(result, buffer.String())
+			buffer.Reset()
+			i += 2
+		} else {
+			buffer.WriteByte(s[i])
+			i++
+		}
+	}
+	return result
+}
