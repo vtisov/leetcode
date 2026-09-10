0853ce2e0b9e1d41bf6711981e698ae5fef9aeaf
vtisov.dev <vtisov.dev@gmail.com>
2026-09-10 13:14:05 +0300

    add add-two-numbers.go

---
+++ src/ru/edu/vtisov/list/add-two-numbers.go
@@ -0,0 +1,29 @@
+package list
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	cur := &ListNode{}
+	fake := cur
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		cur.Next = &ListNode{Val: sum % 10}
+		cur = cur.Next
+		carry = sum / 10
+	}
+	return fake.Next
+}
